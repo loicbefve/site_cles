d8209a01e6ea7a87310487e6ac5c47046a3999eb
loicbefve <befve.loic@gmail.com>
2018-01-24 18:54:06 +0100

    Première utilisation de twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,9 +9,10 @@
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -24,6 +25,8 @@ class ArticleController
      * @Route("/news/{slug}")
      */
     public function show($slug){
-        return new Response($slug);
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('_',' ',$slug))
+        ]);
     }
 }
